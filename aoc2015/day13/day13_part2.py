37784e208e6782e2363998f5b572725b37d8c7b3
Sven Grübel <sven.gruebel@gmx.ch>
2020-03-29 09:43:07 +0200

    Add solution of day 13 of 2015

---
+++ aoc2015/day13/day13_part2.py
@@ -0,0 +1,41 @@
+from itertools import permutations
+from os.path import join, dirname, realpath
+
+happiness_map = dict()
+
+with open(join(dirname(realpath(__file__)), "input.txt")) as f:
+    for line in f:
+        line_split = line.strip()[:-1].split()
+        person_a = line_split[0]
+        person_b = line_split[-1]
+        happiness_change = int(line_split[3])
+
+        if line_split[2] == "lose":
+            happiness_change *= -1
+
+        if person_a not in happiness_map:
+            happiness_map[person_a] = dict()
+
+        happiness_map[person_a][person_b] = happiness_change
+
+# Add myself to list
+for person_a in happiness_map:
+    happiness_map[person_a]["Me"] = 0
+happiness_map["Me"] = dict()
+for person_a in happiness_map:
+    happiness_map["Me"][person_a] = 0
+
+candidates = permutations(happiness_map.keys())
+max_happiness = 0
+
+for candidate in candidates:
+    candidate_happiness = 0
+    candidate_count = len(candidate)
+    for i in range(candidate_count):
+        candidate_happiness += happiness_map[candidate[i]][candidate[(i - 1) % candidate_count]] +\
+                               happiness_map[candidate[i]][candidate[(i + 1) % candidate_count]]
+
+    if candidate_happiness > max_happiness:
+        max_happiness = candidate_happiness
+
+print(max_happiness)
