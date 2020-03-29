596613623b704db214215c964f4f201ece56e9dd
Sven Grübel <sven.gruebel@gmx.ch>
2020-03-29 11:08:15 +0200

    Add solution of day 15 of 2015

---
+++ aoc2015/day15/day15_part2.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+from itertools import product, ifilter
+from os.path import join, dirname, realpath
+
+Ingredient = namedtuple("Ingredient", ["capacity", "durability", "flavor", "texture", "calories"])
+ingredients = []
+
+with open(join(dirname(realpath(__file__)), "input.txt")) as f:
+    for line in f:
+        line_split = line.strip().split()
+        ingredients.append(Ingredient(
+            capacity=int(line_split[2][:-1]),
+            durability=int(line_split[4][:-1]),
+            flavor=int(line_split[6][:-1]),
+            texture=int(line_split[8][:-1]),
+            calories=int(line_split[-1])
+        ))
+
+max_value = 0
+candidates = ifilter(lambda x: sum(x) == 100, product(range(101), repeat=len(ingredients)))
+for candidate in candidates:
+    candidate_cookie = Ingredient(0, 0, 0, 0, 0)
+    for i in range(len(candidate)):
+        candidate_cookie = Ingredient(
+            candidate_cookie.capacity + candidate[i] * ingredients[i].capacity,
+            candidate_cookie.durability + candidate[i] * ingredients[i].durability,
+            candidate_cookie.flavor + candidate[i] * ingredients[i].flavor,
+            candidate_cookie.texture + candidate[i] * ingredients[i].texture,
+            candidate_cookie.calories + candidate[i] * ingredients[i].calories
+        )
+
+    if candidate_cookie.calories != 500:
+        continue
+
+    if any(x <= 0 for x in candidate_cookie):
+        continue
+
+    candidate_value = reduce(lambda x, y: x * y, candidate_cookie) / candidate_cookie.calories
+
+    if candidate_value > max_value:
+        max_value = candidate_value
+
+print(max_value)
